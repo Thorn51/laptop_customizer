42f0b2951ae50bb5aa06cc045525633c26f0b6a0
Matt Thornberry <mtberry16@hotmail.com>
2019-10-11 09:08:38 -0600

    Import cost components

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,4 +1,9 @@
 import React from "react";
+import "./Cart.css";
+import ProcessorCost from "../ProcessorCost/ProcessorCost";
+import OperatingSystemCost from "../OperatingSystemCost/OperatingSystemCost";
+import VideoCardCost from "../VideoCardCost/VideoCardCost";
+import DisplayCost from "../DisplayCost/DisplayCost";
 
 export default class Cart extends React.Component {
   render() {
