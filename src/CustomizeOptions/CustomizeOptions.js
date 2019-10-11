eda7192c40d9a74b44a98310c96079ca9e23fb59
Matt Thornberry <mtberry16@hotmail.com>
2019-10-11 09:23:09 -0600

    Import components to CustomizeOptions

--- src/CustomizeOptions/CustomizeOptions.js
+++ src/CustomizeOptions/CustomizeOptions.js
@@ -1,4 +1,9 @@
 import React from "react";
+import "./CustomizeOptions.css";
+import ProcessorOption from "../ProcessorOption/ProcessorOption";
+import OperatingSystemOption from "../ProcessorOption/ProcessorOption";
+import VideoCardOption from "../VideoCardOption/VideoCardOption";
+import DisplayOption from "../DisplayOption/DisplayOption";
 
 export default class CustomizeOptions extends React.Component {
   render() {
