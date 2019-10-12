9ee620c38cbe75e9efb20bce14b3e526b68f6f87
Matt Thornberry <mtberry16@hotmail.com>
2019-10-11 22:07:54 -0600

    Add option components to CustomizeOptions

--- src/CustomizeOptions/CustomizeOptions.js
+++ src/CustomizeOptions/CustomizeOptions.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "./CustomizeOptions.css";
 import ProcessorOption from "../ProcessorOption/ProcessorOption";
-import OperatingSystemOption from "../ProcessorOption/ProcessorOption";
+import OperatingSystemOption from "../OperatingSystemOption/OperatingSystemOption";
 import VideoCardOption from "../VideoCardOption/VideoCardOption";
 import DisplayOption from "../DisplayOption/DisplayOption";
 
 export default class CustomizeOptions extends React.Component {
   render() {
-    return null;
+    return (
+      <form className="main__summary">
+        <h2>Customize your Laptop</h2>
+        <ProcessorOption
+          processorOptions={this.props.features.Processor}
+          onSelect={this.props.onSelect}
+        />
+        <OperatingSystemOption
+          operatingSystem={this.props.features["Operating System"]}
+          onSelect={this.props.onSelect}
+        />
+        <VideoCardOption
+          videoCard={this.props.features["Video Card"]}
+          onSelect={this.props.onSelect}
+        />
+        <DisplayOption
+          display={this.props.features.display}
+          onSelect={this.props.onSelect}
+        />
+      </form>
+    );
   }
 }
